1216d1373c3c74f59421c4333ad2450c780d61cb
BT_1N3_22@BKTECH.LOCAL <BT_1N3_22@BKTECH.LOCAL>
2018-09-26 09:21:47 -0400

    Initial commit

---
+++ src/CLam/CLamLib.java
@@ -0,0 +1,20 @@
+package CLam;
+
+public class CLamLib
+{
+    public static void main(String[] augs)
+    {
+        String str1 = "LoveJava";
+        while(str1.length() > 0)
+        {
+            System.out.println(str1);
+            str1 = str1.substring(1,str1.length()-1);
+        }
+        String str2 = "What?";
+        for (int i = 0; i < str2.length(); i++)
+        {
+            System.out.println(str2);
+            str2 = str2.substring(str2.length()-1) + str2.substring(0,str2.length()-1);
+        }
+    }
+}
